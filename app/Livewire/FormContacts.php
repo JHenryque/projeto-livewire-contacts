f4da6cfd86a2ce5e16fc6a759a26ac956048937a
JHenrique <henryque.pc.game@gmail.com>
2025-07-21 19:51:58 -0300

    Validação do Formulário de Novo Contacto

--- app/Livewire/FormContacts.php
+++ app/Livewire/FormContacts.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContacts extends Component
 {
+    public $name, $email, $phone;
+
+    public function newContact()
+    {
+        // validation
+        $this->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|min:3|max:50',
+            'phone' => 'required|min:3|max:50',
+        ]);
+
+        // temporary store in log file
+        Log::info('novo contacto: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
+    }
     public function render()
     {
         return view('livewire.form-contacts');
