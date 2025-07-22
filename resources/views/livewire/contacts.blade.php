6b9133a5394a8e7037f03bcec5460afc565664a5
JHenrique <henryque.pc.game@gmail.com>
2025-07-22 12:17:45 -0300

    Apresentar os Contactos

--- resources/views/livewire/contacts.blade.php
+++ resources/views/livewire/contacts.blade.php
@@ -1,6 +1,16 @@
 <div class="card p-5">
-    <p>Contacts</p>
-    <hr class="m-0 p-0">
+    <p class="mb-3">Contacts</p>
+
+                <hr class="m-0 p-0">
+    @foreach($contacts as $contact)
+        <div class="card p-3 mb-1">
+            <div class="row">
+                <div class="col-3">Name: {{ $contact->name }}</div>
+                <div class="col">E-mail: {{ $contact->email }}</div>
+                <div class="col-4">Phone: {{ $contact->phone }}</div>
+            </div>
+        </div>
+    @endforeach
 
 
 </div>
