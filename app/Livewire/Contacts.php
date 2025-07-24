604c8ed617c01f5cc3fe5b1bc280d156f4da4903
JHenrique <henryque.pc.game@gmail.com>
2025-07-24 16:56:42 -0300

    Paginação da Lista de Contactos

--- app/Livewire/Contacts.php
+++ app/Livewire/Contacts.php
@@ -5,28 +5,29 @@ namespace App\Livewire;
 use App\Models\Contact;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Contacts extends Component
 {
-    public $contacts;
+//    public $contacts;
+//    public function mount()
+//    {
+//        $this->contacts = Contact::all();
+//    }
 
-    public function mount()
-    {
-        $this->contacts = Contact::all();
-    }
-
+    use withPagination;
     #[On('contactAdded')]
     public function updateContactList()
     {
-        $this->updateContact();
+        //$this->updateContact();
     }
 
-    public function updateContact()
-    {
-        $this->contacts = Contact::all();
-    }
+//    public function updateContact()
+//    {
+//        $this->contacts = Contact::all();
+//    }
     public function render()
     {
-        return view('livewire.contacts');
+        return view('livewire.contacts')->with('contacts', Contact::paginate(3));
     }
 }
